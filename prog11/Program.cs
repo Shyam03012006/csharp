7d71c7064f9ea9c3ccb4fe80bbb03b39ea6f4fa3
Shyam03012006 <shyam.bhimani124852@marwadiuniversity.ac.in>
2026-03-02 10:15:01 +0530

    Add files via upload

---
+++ prog11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end;
+            Console.Write("starting range:");
+            start = int.Parse(Console.ReadLine());
+            Console.Write("ending range:");
+            end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                int a = 0;
+                for (int j = 2; j <= i; j++)
+                {
+                    if(j%i == 0)
+                    {
+                        a++;
+                        Console.WriteLine(a);
+                        break;
+                    }
+                    else if(j == start)
+                    {
+                        Console.WriteLine("prime numbers:" + i);
+                        break;
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ prog12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int no = int.Parse(Console.ReadLine());
+            int m;
+            int sum = 0;
+
+            while (no > 0)
+            {
+                m = no % 10;
+                no =  no/10;
+                sum = sum + m;
+               
+            }
+            Console.WriteLine("sum=" + sum);
+        }
+    }
+}
