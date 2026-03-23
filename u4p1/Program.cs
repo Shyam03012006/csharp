2cb433626c2e4555e3cf4e5fa01e9491fd1a171c
Shyam2006 <shyam.bhimani124852@marwadiuniversity.ac.in>
2026-03-23 10:30:22 +0530

    Add files via upload

---
+++ u4p1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using u4p1;
+
+namespace unit1program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle r1 = new rectangle();
+            r1.cal();
+            r1.dis();
+            rectangle r2 = new rectangle(30,40);
+            r2.cal();
+            r2.dis();
+        }
+    }
+}
---
+++ u4p1/rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u4p1
+{
+    internal class rectangle
+    {
+        int l, w, area;
+
+        public rectangle()
+        {
+            l = 10;w = 20;
+
+        }
+        public rectangle(int l1, int w1)
+        {
+            l = l1;
+            w = w1;
+
+        }
+
+        public void cal()
+        {
+            area = l * w;
+
+        }
+
+        public void dis()
+        {
+            Console.WriteLine("AREA: " + area);
+        }
+
+    }
+}
