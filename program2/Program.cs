980fb93d37371c3b0910cf971ee65ea013ca527d
Shyam2006 <shyam.bhimani124852@marwadiuniversity.ac.in>
2026-03-23 10:31:05 +0530

    Add files via upload

---
+++ program2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float p, r, n, i;
+
+            Console.Write("enter principal value");
+            p = float.Parse(Console.ReadLine());
+
+            Console.Write("enter principal value");
+            r = float.Parse(Console.ReadLine());
+
+            Console.Write("enter principal value");
+            n = float.Parse(Console.ReadLine());
+
+            i = p * r * n;
+            i = i / 100;
+
+            Console.WriteLine("simple intrest" + i);
+        }
+    }
+}
---
+++ program3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program for calculating compound interest");
+            float p, r, n, i,amount;
+
+            Console.Write("enter value of principal: ");
+            p = float.Parse(Console.ReadLine());
+
+            Console.Write("enter value of rate of interest: ");
+            r = float.Parse(Console.ReadLine());
+
+            Console.Write("enter value for number of year: ");
+            n = float.Parse(Console.ReadLine());
+
+            amount = p;
+            for(int si = 0; si<n; si++)
+            {
+                amount += amount * (r / 100);
+            }
+            i = amount - p;
+
+            Console.WriteLine("compound interest: " + i);
+        }
+    }
+}
